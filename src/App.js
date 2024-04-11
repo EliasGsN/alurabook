5577d98649ec6c3734d08e0bff12f4c8612a5512
gabrielsilva20012005@hotmail.com <gabrielsilva20012005@hotmail.com>
2024-04-11 13:38:31 -0300

    Usando loops

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
-import Logo from './components/Logo';
+import "./App.css";
+import Logo from "./components/Logo";
+import perfil from "./images/perfil.svg"
+import sacola from "./images/sacola.svg"
+
+const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "MINHA ESTANTE"];
+const icons = [perfil, sacola]
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <Logo></Logo>
+        <ul className="opcoes">
+          {textoOpcoes.map((texto) => (
+            <li className="opcao">
+              <p>{texto}</p>
+            </li>
+          ))}
+        </ul>
+        <ul className="icons">
+            {icons.map((icon) => (
+              <li className="icon"><img src={icon}></img></li>
+            ))}
+        </ul>
       </header>
     </div>
   );
