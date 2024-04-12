118d473ae684b1ba7068ef808d64f9a9435de11a
gabrielsilva20012005@hotmail.com <gabrielsilva20012005@hotmail.com>
2024-04-12 13:17:18 -0300

    Trocando arquivos CSS

--- src/components/IconsHeader/index.js
+++ src/components/IconsHeader/index.js
@@ -1,16 +1,26 @@
-import './style.css'
 import perfil from "../../images/perfil.svg"
 import sacola from "../../images/sacola.svg"
+import styled from 'styled-components'
+
+const Icons = styled.ul`
+    display: flex;
+    align-items: center;
+`
+const Icon = styled.li`
+    margin-right: 40px;
+    width: 25px;
+    cursor: pointer;
+`
 
 const icons = [perfil, sacola]
 
 function IconsHeader() {
     return (
-        <ul className="icons">
+        <Icons>
             {icons.map((icon) => (
-                <li className="icon"><img src={icon}></img></li>
+                <Icon><img src={icon}></img></Icon>
             ))}
-        </ul>
+        </Icons>
     )
 }
 
