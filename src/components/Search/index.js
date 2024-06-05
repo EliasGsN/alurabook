5009ae0d95b4efea068e7d91c34f7dcb1182dc66
EliasGsN <gabrielsilva20012005@hotmail.com>
2024-06-05 17:26:24 -0300

    chore: Update Favorites component to fetch and display favorite books

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -43,9 +43,13 @@ function Search() {
     const [livros, setLivros] = useState([])
 
     useEffect(() => {
-       const livrosDaAPI = getLivros()
-       setLivros(livrosDaAPI)
+        fetchLivros();
     }, [])
+
+    async function fetchLivros() {
+        const livrosDaAPI = await getLivros();
+        setLivros(livrosDaAPI);
+    }
 
     function doResearch(evento) {
         const textoDigitado = evento.target.value
